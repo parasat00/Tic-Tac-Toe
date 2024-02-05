d9afd49d08cb224a78c9256c3042cfe1761ac7c5
Parasat Zhangbyrbay <63976887+parasat00@users.noreply.github.com>
2024-02-06 02:55:34 +0600

    update

--- index.js
+++ index.js
@@ -12,19 +12,25 @@ let que = 1;
 function checkForWin() {
  let letter = que % 2 === 1 ? 'x' : 'o';
  let win = false;
+ let stillGoing = false;
 
  for(let i = 0; i < 3; i++) {
   if(poses[i].toString() === `${letter},${letter},${letter}`) { win = true; break;}
 
   if(`${poses[0][i]},${poses[1][i]},${poses[2][i]}` === `${letter},${letter},${letter}`) { win = true; 
   break; }
+
+  if(poses[i].includes('-')) {
+   stillGoing = true;
+  }
  }
 
  if(`${poses[0][0]},${poses[1][1]},${poses[2][2]}` === `${letter},${letter},${letter}` || `${poses[0][2]},${poses[1][1]},${poses[2][0]}` === `${letter},${letter},${letter}`) { win = true; }
 
- if(!win) { return }
- 
- modal.children[0].textContent = `Player-${que % 2 == 1 ? 1 : 2} won!`;
+ if(!win && stillGoing) { return }
+
+ const finishText = win ? `Player-${que % 2 == 1 ? 1 : 2} won!` : 'This is a Tie'
+ modal.children[0].textContent = finishText;
  modal.classList.remove('hide');
 }
 
